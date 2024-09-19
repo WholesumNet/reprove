5a03f756d4d897245693224e7567ebb7279ca934
rezahsnz <rezahsnz@proton.me>
2024-09-19 18:19:36 +0330

    features: recursive proving of segments.

--- README.md
+++ README.md
@@ -2,4 +2,19 @@
 
 Wholesum network is a p2p verifiable computing network `tailored for ETH L2 sequencer proving`. It builds on top of [Risc0](https://risczero.com/), [Libp2p](https://libp2p.io), and decentralized storage options like [Swarm](https://ethswarm.org) and Filecoin to facilitate verifiable computing at scale. The design of the network follows a p2p parallel proving scheme where Risc0 jobs are passed around, proved, and finally combined into a final proof ready for L1 verification.
 
-`provelift` is used by servers to prove *segmented* Risc0 guests. Given a [Segment](https://docs.rs/risc0-zkvm/latest/risc0_zkvm/struct.Segment.html) as input, it is first *proved* to obtain a [SegmentReceipt](https://docs.rs/risc0-zkvm/latest/risc0_zkvm/struct.SegmentReceipt.html). Then, the `SegmentReceipt` is *lifted* to obtain a [SuccinctReceipt](https://docs.rs/risc0-zkvm/latest/risc0_zkvm/struct.SuccinctReceipt.html). 
+`recurse` is used by servers to prove *segmented* Risc0 guests. It's a helper program for recursive proving.
+
+### USAGE
+<pre>
+Usage: recurse [COMMAND]
+
+Commands:
+  prove  proves a segment, and then lifts it
+  join   joins two SuccinctReceipts
+  snark  extract a Groth16 snark
+  help   Print this message or the help of the given subcommand(s)
+
+Options:
+  -h, --help     Print help
+  -V, --version  Print version
+</pre>
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,188 @@
-fn main() {
-    println!("Hello, world!");
+use risc0_zkvm::{
+    ProverOpts, 
+    ApiClient,
+    Asset, AssetRequest,
+    SuccinctReceipt, 
+    Receipt, ReceiptClaim,
+};
+use std::{
+    fs,
+    path::PathBuf,
+    time::{Instant},
+};
+use anyhow;
+
+use clap::{Parser, Subcommand};
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// proves a segment, and then lifts it
+    Prove {
+        /// input Segment blob
+        #[arg(short, long)]
+        in_seg: String,
+
+        /// file to write lifted SuccinctReceipt to
+        #[arg(short, long)]
+        out_sr: String,
+    },
+
+    /// joins two SuccinctReceipts
+    Join {
+        /// left SuccinctReceipt
+        #[arg(short, long)]
+        left_sr: String,
+
+        /// right SuccinctReceipt
+        #[arg(short, long)]
+        right_sr: String,
+
+        /// file to write the resulting joined SuccinctReceipt to
+        #[arg(short, long)]
+        out_sr: String,
+    },
+
+    /// extract a Groth16 snark
+    Snark {
+        /// input SuccinctReceipt
+        #[arg(short, long)]
+        in_sr: String,
+
+        /// file to write the Groth16 receipt to
+        #[arg(short, long)]
+        out_receipt: String,
+    },
+
+}
+
+
+fn main() -> anyhow::Result<()> {
+    let args = Cli::parse();
+    match &args.command {
+        Some(Commands::Prove{in_seg, out_sr}) => {
+            let _sr = prove_and_lift(
+                PathBuf::from(in_seg),
+                PathBuf::from(out_sr),
+            )?;
+        },
+
+        Some(Commands::Join {left_sr, right_sr, out_sr}) => {
+            let _sr = join(
+                PathBuf::from(left_sr),
+                PathBuf::from(right_sr),
+                PathBuf::from(out_sr),
+            )?;
+        },
+        
+        Some(Commands::Snark {in_sr, out_receipt}) => {
+            let _r = to_snark(
+                PathBuf::from(in_sr),
+                PathBuf::from(out_receipt),
+            )?;
+        },
+
+        _ => {}
+    }
+
+    Ok(())
+}
+
+
+fn prove_and_lift(
+    in_seg_path: PathBuf,
+    out_sr_path: PathBuf,
+) -> anyhow::Result<SuccinctReceipt<ReceiptClaim>> {
+    let r0_client = ApiClient::from_env()?;
+    let prover_opts = ProverOpts::default();
+    // fisrt prove
+    let mut now = Instant::now();
+    let segment_receipt = r0_client
+        .prove_segment(
+            &prover_opts,
+            Asset::Path(in_seg_path),
+            AssetRequest::Inline,
+    )?; 
+    let prove_dur = now.elapsed().as_secs();
+    // and then lift
+    let asset = Asset::try_from(segment_receipt)?.as_bytes()?;
+    now = Instant::now();
+    let succinct_receipt = r0_client
+        .lift(
+            &prover_opts,
+            Asset::Inline(asset),
+            AssetRequest::Inline
+        )?;
+    let lift_dur = now.elapsed().as_secs();
+    println!("prove took `{} secs`, and lift `{} secs`.", prove_dur, lift_dur);
+    let _ = fs::write(
+        &out_sr_path,
+        bincode::serialize(&succinct_receipt)?
+    );
+    Ok(succinct_receipt)
+}
+
+fn join(
+    left_sr_path: PathBuf,
+    right_sr_path: PathBuf,
+    out_sr_path: PathBuf,
+) -> anyhow::Result<SuccinctReceipt<ReceiptClaim>> {
+    let r0_client = ApiClient::from_env()?;
+    let prover_opts = ProverOpts::default();
+
+    let now = Instant::now();
+    let succinct_receipt = r0_client
+        .join(
+            &prover_opts,
+            Asset::Path(left_sr_path),
+            Asset::Path(right_sr_path),
+            AssetRequest::Inline,
+        )?;
+    let dur = now.elapsed().as_secs();
+    println!("join took `{dur} secs`");
+    let _ = fs::write(
+        &out_sr_path,
+        bincode::serialize(&succinct_receipt)?
+    );
+    Ok(succinct_receipt)
+}
+
+fn to_snark(
+    in_sr_path: PathBuf,
+    out_receipt_path: PathBuf,
+) -> anyhow::Result<Receipt> {
+    let r0_client = ApiClient::from_env()?;
+    let prover_opts = ProverOpts::default();
+    // fist transform via identity_p254
+    let now = Instant::now();
+    let succinct_receipt = r0_client
+        .identity_p254(
+            &prover_opts,
+            Asset::Path(in_sr_path),
+            AssetRequest::Inline,
+        )?;
+    let dur = now.elapsed().as_secs();
+    println!("identity_p254 took `{dur} secs`");
+    // and then extract the compressed snark(Groth16)
+    let asset = Asset::try_from(succinct_receipt)?.as_bytes()?;
+    let now = Instant::now();
+    let receipt = r0_client
+        .compress(
+            &prover_opts,
+            Asset::Inline(asset),
+            AssetRequest::Inline,
+        )?;
+    let dur = now.elapsed().as_secs();
+    println!("compress took `{dur} secs`");
+    let _ = fs::write(
+        &out_receipt_path,
+        bincode::serialize(&receipt)?
+    );
+    println!("your Groth16 receipt is ready!");
+    Ok(receipt)
 }
